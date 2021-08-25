5319ac23bb12b5aa94b1b22dafd7d5acda6e506f
egiegarciano14 <egie.garciano@sun-asterisk.com>
2021-08-25 16:06:51 +0800

    display user lists

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import Header from './Header';
 import Dashboard from './pages/Dashboard';
 import WordsLearned from './pages/WordsLearned';
 import Category from './pages/Category';
+import UserList from './pages/UserList';
+import UserProfile from './pages/UserProfile';
 
 const App = () => {
   return (
     <div className='ui container'>
-      <BrowserRouter>
-        <div>
-          <Header />
+      <Router>
+        <Header />
+        <Switch>
           <Route path='/' exact component={Dashboard} />
           <Route path='/words-learned' component={WordsLearned}></Route>
           <Route path='/categories' component={Category}></Route>
-        </div>
-      </BrowserRouter>
+          <Route path='/categories' component={Category}></Route>
+          <Route path='/user-list' component={UserList}></Route>
+          <Route path='/user-profile/:id' component={UserProfile}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
