84d39282ddf3bbb6330f8b0b124d8c28d8f39374
egiegarciano14 <egie.garciano@sun-asterisk.com>
2021-09-01 16:03:45 +0800

    create register page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 
 import Header from './Header';
 import Dashboard from './pages/Dashboard';
@@ -7,13 +8,21 @@ import WordsLearned from './pages/WordsLearned';
 import Category from './pages/Category';
 import UserList from './pages/UserList';
 import UserProfile from './pages/UserProfile';
+import Register from './pages/Register';
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+
+axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
     <div className='ui container'>
       <Router>
-        <Header />
         <Switch>
+          <Route path='/register' exact component={Register} />
+          <Header />
           <Route path='/' exact component={Dashboard} />
           <Route path='/words-learned' component={WordsLearned}></Route>
           <Route path='/categories' component={Category}></Route>
